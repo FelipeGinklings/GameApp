bd35698cba096021aa037634f7bc553fc6c76040
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-12-19 10:55:48 -0300

    Respecting Device Screen Restrictions with the SafeAreaView

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 
@@ -21,9 +22,8 @@ const App = () => {
 			source={require('./assets/images/background.png')}
 			resizeMode="cover"
 			style={styles.rootScreen}
-			imageStyle={styles.backgroundImage}
 		>
-			{screen}
+			<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
 		</ImageBackground>
 	);
 };
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,7 +7,7 @@ const GameScreen = () => {
 			{/* GUESS */}
 			<View>
 				<Text>Higher or lower?</Text>
-        {/* + - */}
+				{/* + - */}
 			</View>
 			{/* <View>LOG ROUNDS</View> */}
 		</View>
@@ -19,6 +19,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
-		padding: 12,
+		padding: 32,
 	},
 });
